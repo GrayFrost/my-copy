d0339ac21197fbff86696dcfadb9a433394eba62
zzh <1760655799@qq.com>
2024-03-23 21:54:48 +0800

    feat: style

--- src/popup/App.tsx
+++ src/popup/App.tsx
@@ -1,9 +1,18 @@
-import '../global.css';
-import "./App.css";
+import "../global.css";
 import { Button } from "@/components/ui/button";
-
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { Label } from "@/components/ui/label";
+import { Settings, Files, Trash2 } from "lucide-react";
 
 export default function App() {
+
   const openSettings = () => {
     if (chrome.runtime.openOptionsPage) {
       chrome.runtime.openOptionsPage();
@@ -11,10 +20,36 @@ export default function App() {
       window.open(chrome.runtime.getURL("options/index.html"));
     }
   };
+
   return (
-    <div className="popup">
-      <h1>popup</h1>
-      <Button onClick={openSettings}>open settings</Button>
+    <div className="w-[750px] h-[500px]">
+      <header className="h-12 flex justify-between items-center px-4">
+        <Label>My Copy</Label>
+        <Settings onClick={openSettings} className="hover:cursor-pointer"/>
+      </header>
+      <div className="rounded-md border">
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead className="w-[100px]">Content</TableHead>
+              <TableHead>Time</TableHead>
+              <TableHead>Method</TableHead>
+              <TableHead className="text-right">Actions</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            <TableRow>
+              <TableCell className="font-medium">INV001</TableCell>
+              <TableCell>Paid</TableCell>
+              <TableCell>Credit Card</TableCell>
+              <TableCell className="flex justify-end gap-1">
+                <Files className="hover:cursor-pointer hover:text-green-400"/>
+                <Trash2 className="hover:cursor-pointer hover:text-rose-400" />
+              </TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+      </div>
     </div>
   );
 }
--- src/popup/main.tsx
+++ src/popup/main.tsx
@@ -1,5 +1,7 @@
 import * as ReactDOM from "react-dom/client";
 import App from "./App";
 
-const root = ReactDOM.createRoot(document.querySelector("#popup-root"));
+const root = ReactDOM.createRoot(
+  document.querySelector("#popup-root") || document.body
+);
 root.render(<App />);
